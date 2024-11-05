aa78147d0234ec7ae2765c1e021234b28ea09602
Bastian Arenas <bastian.arenas.z95@gmail.com>
2024-11-05 00:03:55 -0300

    Se creo pagina inicio sesion y ahora se crea perfil

---
+++ src/app/iniciar-sesion/iniciar-sesion.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { AuthService } from './../servicio/auth/auth.service';
+
+@Component({
+  selector: 'app-iniciar-sesion',
+  templateUrl: './iniciar-sesion.page.html',
+  styleUrls: ['./iniciar-sesion.page.scss'],
+})
+export class IniciarSesionPage implements OnInit {
+  public formularioInicioSesion : FormGroup;
+  constructor(
+    private formBuilder : FormBuilder,
+    public auth : AuthService
+  ) {
+    this.formularioInicioSesion = formBuilder.group({
+      usuario: ['emmaj',
+                [ Validators.required,
+                      Validators.minLength(3),
+                      Validators.maxLength(10)
+                ]],
+      password: ['emmajpass',
+                  [ Validators.required,
+                    Validators.minLength(3),
+                    Validators.maxLength(10)
+                ] ]
+    });
+  }
+  public iniciarSesion(){
+    if(!this.formularioInicioSesion.valid){
+      alert(" esta incorrecto");
+      this.formularioInicioSesion.controls['usuario'].setValue("");
+      this.formularioInicioSesion.controls['password'].setValue("");
+      this.formularioInicioSesion.clearValidators();
+      return
+    }
+    this.auth.iniciarSesion(
+      this.formularioInicioSesion.controls["usuario"].value, // usuario
+      this.formularioInicioSesion.controls["password"].value // password
+    );
+  }
+  ngOnInit() {
+  }
+
+}
+
